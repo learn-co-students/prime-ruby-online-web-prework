9135cf4d7cd2e1bfbdf5929b52b75c4a092996e7
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-13 22:49:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -3,13 +3,14 @@
 def prime?(num)
   half_num = num/2 
   is_it_prime = true
-  if num < 3 
-    return is_it_prime
-  end 
+  if num < 2
+    return is_it_prime = false    
   else 
     for i in 2..half_num
      if num % i == 0 
        return is_it_prime = false
      end 
     end 
+  end 
+  is_it_prime
 end 
