f8685fbe56aa1e39a95c66184d50ab5b49503fd0
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-13 22:40:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(num)
+  half_num = num/2 
+  is_it_prime = true
+  if num < 3 
+    return is_it_prime
+  end 
+  else 
+    for i in 2..half_num
+     if num % i == 0 
+       return is_it_prime = false
+     end 
+    end 
+end 
