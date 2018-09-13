cf85df12743d8e19edf4d29475716c675189fbf5
Gina Fedder <ginafedder@yahoo.com>
2018-09-13 21:19:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+
+def prime?(integer)
+ #can only be divided by itself and 1 
+ if integer < 2 
+   return false
+ end
+ for n in 2..(integer - 1)
+  if integer % n == 0 
+   return false 
+  end 
+ end 
+ true 
+end
