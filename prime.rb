8b3b24403cefacbda85e13761867bbbdfa303d23
Matthew McCormack <mmccormack1182@gmail.com>
2018-09-18 14:06:51 -0400

    Tests Passed

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime?(integer)
+  prime_array = []
+  if integer <= 1
+    return false
+  end
+  range = (1..integer).to_a
+  range.each do |number|
+    if integer % number == 0
+      prime_array.push (number)
+    end
+  end
+  if prime_array.length > 2
+    return false
+  else
+    return true
+  end
+end
