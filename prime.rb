4dc5360df20ae9edf3d111b7792b77fcee0818ad
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-21 18:18:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(num)
+    return FALSE if num < 2
+    (2..num - 1).each do |x|
+        if (num % x) == 0
+            return FALSE
+        end
+    end
+    return TRUE
+end
