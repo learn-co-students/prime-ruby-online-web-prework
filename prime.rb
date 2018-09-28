b20fd0c679587c0f35b55876aa37ca63508522c6
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-28 03:20:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  is_prime = true
+  range = 2..number-1
+  array = []
+  if number == -1 || number == 0 || number == 1
+    is_prime = false
+  end
+  range.each do |num|
+    array << num
+  end
+  array.each do |element|
+  if number % element == 0 
+  is_prime = false
+  end
+  end
+  is_prime
+end
