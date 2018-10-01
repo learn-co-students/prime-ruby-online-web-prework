1392ba24109191806cf90ed7d6d29e12c438c53e
Brandon Teich <brandonteich123@gmail.com>
2018-10-01 05:54:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(integer)
-  if number < 2           # If the num is less than 2 its not prime
-    return false          # return false 
+  if integer < 2           
+    return false          
   end                     
   
-  for i in 2...number     # Now we iterate from 2 to the number itself
-    if number % i == 0    # if the number is divisible by anything counting
-      return false        # to itself then it is not prime  
+  for i in 2..intger     
+    if integer % i == 0   
+      return false
     end
 end
 end
