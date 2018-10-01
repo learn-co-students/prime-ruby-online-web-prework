94d43773a76b552d4391e9d2aa386bb55183f460
Brandon Teich <brandonteich123@gmail.com>
2018-10-01 05:44:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 # Add  code here!
-
 def prime?(integer)
-    (0...integer).each do |x|
-      if integer % x == 0
-        false
-      else
-        true
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
     end
-  end
+    true
 end
