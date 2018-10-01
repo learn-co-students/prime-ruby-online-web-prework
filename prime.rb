502baa9968a4411176cf7c0fce5f50f144d54577
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 15:27:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+  # returns true if integer is prime, else false
+  
+  # 1, -1 and 0 are not prime
+  return false if integer.abs < 2 
+  
+  # 
+  for factor in (2..integer.abs - 1)
+    if integer.abs % factor == 0
+      return false
+    end
+  end
+    true
+end
