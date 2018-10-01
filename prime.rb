2d408ef423edf280d6511dc915d095ace80b23e3
Brandon Teich <brandonteich123@gmail.com>
2018-10-01 05:43:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
     (2..num-1).each do |check|
-      if (num % check).each do |check|
+      (num % check).each do |check|
         if(num % check) == 0
           false
         end
