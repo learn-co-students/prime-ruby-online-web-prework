e4770f16602930def6521fd680bef11334e3263f
Brandon Teich <brandonteich123@gmail.com>
2018-10-01 05:52:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(integer)
   if integer < 2           
     false         
   end     
-  for i in (2...integer)
+  for i in (2...integer-1)
     if integer % i == 0   
       false
     end
