10e69769ec38d5a6edf6b865f6c26952230f5d41
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-09 18:09:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(int)
+  diviserCounter = 0;
+  if int > 1
+    for ele in (1..int)
+      if int % ele == 0
+        diviserCounter += 1
+      end 
+    end
+    
+    if diviserCounter  == 2 
+      return true
+    else 
+      return false
+    end 
+  else 
+    return false
+  end 
+end 
