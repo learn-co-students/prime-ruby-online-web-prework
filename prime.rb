c7dfeecf8e5313b1be241b0a31639c7a75b5b8f9
Steven Ott <stevenkott@icloud.com>
2018-11-13 23:39:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)# Add  code here!
+ if number <= 1
+   return FALSE
+ end 
+ counter = 2
+ while counter < number
+    if (number % counter == 0)
+        return FALSE
+    end
+    counter += 1
+ end
+return TRUE
+end
