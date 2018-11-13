54372a9b6936981f65acb1284b599cd3b98c0781
Britny Edwards <bredwards2@gmail.com>
2018-11-13 01:59:21 +0000

    Done.

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -19,3 +19,13 @@ describe "prime?" do
     expect(prime?(101013)).to be(false)
   end
 end
+
+ def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
+  end
+end
