c69504d3d91e008202ae0ffdb43754e8540d7dcc
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 00:47:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+return false if number < 2
+  (2...number).each do |x|
+      if (number % x) == 0
+        return false
+      end
+     end
+  return true
+end
