0de95f4b2cdcf35e177c15426907ac6b3f52fd2c
Joe Lott <raymond.j.lott@gmail.com>
2018-12-03 02:08:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(n)
+  if n <= 3
+    n > 1
+  elsif n % 2 == 0 || n % 3 == 0
+    false
+  else
+    i = 5
+    while i ** 2 <= n
+      if n % i == 0 || n % (i + 2) == 0
+        return false
+      end
+      i += 6
+    end
+    true
+  end
+end
+
+puts  Benchmark.measure{prime?(2432934)}
