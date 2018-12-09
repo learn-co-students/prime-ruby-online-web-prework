073704b3a9a0fcaea77343006ad4e838f41bab72
Henry Sztanski <henry@sztanski.com>
2018-12-09 22:03:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+
+def prime?(num)
+
+  if num == 0
+    return false
+  elsif num == 1
+    return false
+  elsif num < 0
+    return false
+  end
+
+  for counter in 2..num-1 do
+    if num % counter == 0
+      return false
+    end
+  end
+
+  return true
+end
+
+puts prime?(-5)
