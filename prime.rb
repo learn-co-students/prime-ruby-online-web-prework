414b06d650366d85f5c356975f1fa3b2c5153aad
Kevin Kang <kevinkangg@gmail.com>
2018-12-09 23:57:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
+require "pry"
+
 # Add  code here!
+
+def prime?(num)
+
+  if num <= 0 || num == 1
+    false
+  elsif num == 2 || num == 3
+    true
+  elsif
+    square_root = Math.sqrt(num.abs).floor
+    square_array = (2..square_root).to_a
+
+    root_array = square_array.map {|square| num % square}
+
+    if root_array.any? {|num| num == 0} == true
+      false
+    elsif root_array.any? {|num| num == 0} == false
+      true
+    end
+  end
+end
