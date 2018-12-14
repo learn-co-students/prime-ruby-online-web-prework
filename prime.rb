1b1d3e25317409a0f01d021095266f2977f43577
s greenbaum <gbs4ever2@gmail.com>
2018-12-14 20:32:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+lopp
+n=2
+  while num  % n = prime
+  if prime== 0
+    break
+  elsif n>= num
+    puts "#{num}is prime"
+
+end
+
+end
+#Set N=2.
+#Divide P by N and find the remainder R.
+#If R is zero, P is not prime. Exit.
+#Set N=N+1.
+#If N is greater than or equal P, P is Prime. Exit.
+#Go to step 2.
