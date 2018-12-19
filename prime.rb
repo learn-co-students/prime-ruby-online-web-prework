57164a783ce22e267d477e4010482735c64ecb8b
Luis Zabala <luis.zabala076@gmail.com>
2018-12-19 21:47:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+def prime?(number)
+  if number < 2
+    return false
+end
+number_array = (2..number -1)
+number_array.each do |x|
+  if (number % x) == 0
+    return false
+    end
+  end
+  true
+end
