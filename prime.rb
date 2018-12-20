bb8ae288e91d09e91868585bfef5c97ef6ec5c29
Joan in DC <joanrigdon@gmail.com>
2018-12-20 02:36:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 2
+    return FALSE
+  end
+    
+  myArray = Array (2..9999)
+  list_of_remainders = []
+  myArray.each do |i|
+    result = integer % i
+    if integer != i
+      list_of_remainders.push result
+    end
+  end
+  
+  if list_of_remainders.include? 0
+    return FALSE
+  else
+    return TRUE
+  end
+  
+end
+  
+  
+  
