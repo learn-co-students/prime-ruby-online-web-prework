bbff4f34bef9c1fbdce23e469e6656ce08395955
Derek Cerretani <derekcerretani@gmail.com>
2018-12-20 18:43:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+=begin
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+end
+=end
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  n = 2
+  while n < num
+    return false if num % n == 0
+    n += 1
+  end
+  true
+end
