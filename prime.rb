99477234b6c6b354d3477c5d4d2e91b4b891a421
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-05 06:38:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num.abs == 1 || num == 0
+    return false
+  end
+  (2..num.abs).each do |i|
+    if i == num
+      break
+    elsif num.abs % i == 0
+      return false
+    end
+  end
+  return true
+end
