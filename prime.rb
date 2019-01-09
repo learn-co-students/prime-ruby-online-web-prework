281ec60197ab66d6de386204e53a2a29c3c28409
Eyal Toledano <eyaltoledano@me.com>
2019-01-09 23:52:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -5,16 +5,18 @@
 # How do I know if a number is a multiple of 3?
 # Divisibility by 3 or 9. First, take any number (for this example it will be 492) and add together each digit in the number (4 + 9 + 2 = 15). Then take that sum (15) and determine if it is divisible by 3. The original number is divisible by 3 (or 9) if and only if the sum of its digits is divisible by 3 (or 9).
 
-number = 5
-numbers = (2..number).to_a # makes an array as with as many elements as the number
+number = 492
+# numbers = (2..number).to_a
 
-def is_multiple?(number)
-  numbers = Array.new(number)
-  numbers.each do |number|
-    numbers << "#{number}"
-    number = number - 1
-  end
+def split_number_digits_into_array(number)
+  number_array = number.to_s.split('')
 end
+
+def add_digits_together(number)
+  number_array = split_number_digits_into_array(number)
+  number_of_digits = number_array.size
+end
+
 
 
 def prime?(number)
