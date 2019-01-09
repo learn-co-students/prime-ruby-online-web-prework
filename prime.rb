a62157ebc9d8a34519e6cbd8f3851d4273276c79
Eyal Toledano <eyaltoledano@me.com>
2019-01-09 03:39:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+#  See if an integer n can be divided by each number in turn that is less than n
+
+# A simple but slow method of checking the primality of a given number n, called trial division, tests whether n is a multiple of any integer between 2 and square root of n
+
+# How do I know if a number is a multiple of 3?
+# Divisibility by 3 or 9. First, take any number (for this example it will be 492) and add together each digit in the number (4 + 9 + 2 = 15). Then take that sum (15) and determine if it is divisible by 3. The original number is divisible by 3 (or 9) if and only if the sum of its digits is divisible by 3 (or 9).
+
+number = 5
+numbers = Array.new(number) # makes an array as with as many elements as the number
+
+def is_multiple?(number)
+  numbers = Array.new(number)
+end
+
+
+def prime?(number)
+
+end
