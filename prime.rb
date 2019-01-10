0206588a725c596644ecbfb133b57280cb53619f
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-10 01:25:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2 
+    return true
+  else
+    numerators = *(2...num)
+    numerators.each do |x|
+        if num % x == 0
+          return false 
+        end
+    end
+    return true
+  end
+end
