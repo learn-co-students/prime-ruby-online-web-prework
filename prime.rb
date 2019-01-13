bb753c35704b0d82cad584eb9f70615ae00b6f33
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-13 20:49:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+
+def prime?(integer)
+
+boolean is_prime == true
+
+  if (integer <= 1)
+    return is_prime == false
+  end
+
+  for i in 2 .. integer-1
+      if(integer % i ==0)
+        return return is_prime == false
+      end
+    end
+ return is_prime
+}
+
+
+end# Add  code here!
