eb66cd3aa6377c6130c6002a8de9a84e8cb327bf
Allen F Partlow <allenfp@gmail.com>
2019-01-24 07:12:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+
+  if num <= 1
+    return false
+  end
+
+  half_num = num / 2
+  num_range = 2..half_num
+
+  num_range.each do |element|
+    if num % element == 0
+      return false
+    end
+  end
+  true
+end
