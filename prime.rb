0d5342ab389b3fbecc78e6b8e813c2534ec9f1ef
Cody Yantis <cyantis@gmail.com>
2019-01-24 00:35:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(i)
+  counter = (2...i).to_a
+    counter.each do |item|
+      if i % item == 0
+        return false
+        break
+      else
+        item +=1
+      end
+    end
+  i < 0 || i == 0 || i == 1 ? false : true
+end
