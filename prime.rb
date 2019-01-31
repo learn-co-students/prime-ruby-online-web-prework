1e799a711eb10f8666fb87e54b2159c12aa6daad
Chase Hilbert <chase@heyspacetime.com>
2019-01-31 06:36:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime?(num)
+
+  if num <= 1
+    return false
+  end
+
+  numbers = 2...num
+  puts numbers.to_a
+
+  numbers.each do |integer|
+
+    if num % integer == 0
+      return false
+    end
+  end
+   true
+ end
