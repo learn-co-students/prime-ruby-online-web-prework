a1de00a95140592db7c8434a32d1756334c5bc9b
Brandi Smith <brandi@Brandis-MacBook-Pro.local>
2019-02-04 10:53:26 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+    if num > 1
+        for i in 2...num do
+            if num % i == 0
+                return false
+            end
+        end
+        return true
+    end
+    false
+end
