ff56c512499a1c65389a46d44b8bac0e2f47e7de
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-04 19:58:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(n)
+ if n <= 1
+   return false
+ else
+   (2..n-1).none? do |divisor|
+     n % divisor == 0  #or (2..n-1).none? {|divisor| n % divisor == 0}
+   end# Add  code here!
+ end
+end
