e6ac67390ebf85878b52b113765d22089a5624ec
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-07 20:12:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num==2 || num==3 
+    return true 
+  elsif num==0 || num==1 || num < 0 
+    return false
+  end
+  # makes an array of nums to be divided
+  num_divided_with = (2...num).to_a 
+  
+  # apply each on array 
+  num_divided_with.each do |y|
+    if num%y == 0 
+      return false  
+    end
+  end
+  return true
+end
