bb9177d3d6177cbb879fd75029d171b5639247c8
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 23:07:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  return false if !number.integer?
+  
+  return false if number < 2
+  
+  (2..number-1).each do |i|
+    # I want to now divide this i by all numbers in the range UP TO i
+    return false if number % i == 0
+  end
+    true
+end
+  
+
