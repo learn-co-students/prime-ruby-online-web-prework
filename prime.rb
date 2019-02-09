b8de9c23b65d3bd25805f1dc1b13fc354c09298b
Kat B <katreesebarnes@gmail.com>
2019-02-09 03:42:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,15 @@
-def prime?(num)
-if prime = num < 2
-return true
-else 
+def prime?(num) 
+  return false if !num.integer?
+  
+  return false if num < 2
+  
+  return true if num == 0
+  
+  (2..num -1).each {|int| return false if num <= 1 || num % int == 0}
+  
+   true
+  
+ 
  end
     
   
