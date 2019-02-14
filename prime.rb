7358d3720b1fb7648af984d4f3426b28440ac3d9
Levik Naparstek <levik010@gmail.com>
2019-02-14 22:05:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,33 @@
 # Add  code here!
+# def prime?(n)
+#   (2..n/2).none?{|i| n % i == 0}
+# end
+
+# def prime?(n)
+#   puts "That's not an integer." unless n.is_a? Integer
+#   is_prime = true
+#   for i in 2..n-1
+#     if n % i == 0
+#       is_prime = false
+#     end
+#   end
+#   if is_prime
+#     puts "#{n} is prime!"
+#   else
+#     puts "#{n} is not prime."
+#   end
+# end
+
+# prime(n)
+
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
+end
+
+
