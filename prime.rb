541b7086648b4b8b6ee331e62a7c89557f3ca858
kyle smith <kylemclellan@gmail.com>
2019-02-16 02:58:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false
+  end
+  if num == 2 || num == 3
+    return true
+  end
+  comp = num/2.0.ceil
+  for i in 2..comp do
+    if num % i == 0 
+      return false 
+    end
+  end
+  true
+end
