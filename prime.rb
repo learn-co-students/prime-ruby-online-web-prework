8bf7ba311d16806709a30d93104eae5ffc315407
Leeya Davis <leeya463@gmail.com>
2019-02-18 15:46:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+num = gets.to_i
+
+def prime?(num)
+  while (num <= 1000)
+    prime_flag = true
+
+    x = 2
+
+    while (x <= num / 2)
+
+      if (num % x == 0)
+        prime_flag = false
+        break
+      end
+
+    x += 1
+    end
+  end
+
+  prime_flag
+
+end
