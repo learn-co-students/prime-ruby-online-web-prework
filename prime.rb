79da12998d0d5bf0592ca0105ba849b53bb2b1a9
Natasha Chernis <natashachernis@gmail.com>
2019-02-18 04:49:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+def prime?(dividend)
+  if dividend < 0
+    return false
+  else
+    nil
+  end
+
+  divisors = []
+  divisors_count = dividend
+  until divisors_count == 0
+    divisors << divisors_count
+    divisors_count -= 1
+  end
+
+  tally = 0
+  divisors.each do |divisor|
+    if dividend % divisor == 0
+      tally += 1
+    else
+      nil
+    end
+  end
+  tally == 2 ? true : false
+end
