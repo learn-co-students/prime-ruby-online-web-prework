d73dee5c8db26485378e06323f2db5973981975e
Darren L <darren.lou@gmail.com>
2019-02-19 07:57:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num == 2 || num == 3
+    return true
+  else
+    for n in (2..num-1) do
+      if num % n == 0
+        return false
+      end
+    end
+  end
+  return true
+end
