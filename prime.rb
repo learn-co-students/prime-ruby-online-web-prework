70780f18ede2739f21e6acba7fc02f305674f9e2
Samuel W Tong <ssamt1142@gmail.com>
2019-02-22 04:51:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require "pry"
+def prime?(n)
+  prime = true
+  if n <= 1
+    return false
+  else  
+    for i in (2..n-1) do
+      if (n % i == 0)
+        return false
+      end
+    end
+  end
+  prime
+end
