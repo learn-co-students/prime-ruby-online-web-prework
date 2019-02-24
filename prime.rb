78de05880da78ae9526b5b2b2c0b2603982de587
Daniel Schneider <daschne8@gmail.com>
2019-02-24 21:00:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  if number == 1 || number <= 0
+    return false
+  end
+  if number == 2
+    return true
+  end
+  range = (number / 2).round
+  for i in 2..range do
+    if number % i == 0
+        return false
+    end
+    i += 1
+  end
+  return true
+end
