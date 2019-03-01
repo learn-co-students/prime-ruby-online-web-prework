6a3cf906988660dbdf75a72b99c44373ffa018f1
Annie Phan <anniephan.atp@gmail.com>
2019-03-01 17:59:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+
+def prime?(int)
+  if int <= 1
+    return false
+  end
+  (2...int).each do |element|
+    if int % element == 0
+      return false
+    end
+  end
+  true
+end
