40df5fe57510fb8fcfdae173061383c12bbaab4b
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-04 22:45:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,18 +1,8 @@
 def prime?(int)
-  if int <= 1
-    is_prime = false
-  elsif int == 2
-    is_prime = true
-  else
-    range = (2..int-1).to_a
-    range.each do |element|
-      if int % element == 0
-        is_prime = false
-        break
-      else
-        is_prime = true
-      end
-    end
+  return false if int <= 1
+  return true if int == 2
+  (2..int-1).each do |element|
+    return false if int % element == 0
   end
-  is_prime
+  true
 end
