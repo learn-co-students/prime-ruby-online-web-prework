3ed24b166a5fb18121413a98abb54e8e0475e9b1
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-06 23:17:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(prime_number)
+  if prime_number == % 1 && % prime_number
+    print "true"
+  else
+    prime_number < 0 or prime_number == 0 or prime_number == 1
+    print "false"
+  end
+end
+
+puts prime?(81)
