a23ec23eeda1631cc90d34dfcfdac8e48321bd8f
Eric Storey <estorey@gmail.com>
2019-03-07 23:55:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(i)
+  i=i.abs
+  max=i/2
+  if max==0 
+    return false
+  end
+  tests=(2..max)
+  tests.to_a
+  tests.each do|j|
+    if i%j==0 
+      return false
+    end
+  end
+  true
+end
