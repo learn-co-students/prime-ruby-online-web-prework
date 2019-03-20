aa3a3d12ffddb33f2e5032f2d63f66357531bd7e
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-20 01:26:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+  array = (2...number).to_a
+  number_of_elements = array.size
+  element_number = 0
+  if number <= 1
+    return false
+  elsif number == 2 || number == 3
+    return true
+  end
+  while element_number < number_of_elements
+    if number % array[element_number] == 0
+      return false
+    end
+    element_number += 1
+  end
+  return true
+end
