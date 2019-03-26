7c7e0e642179fe1eeab551d260d180e096d952b1
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-26 18:42:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -7,37 +7,3 @@ def prime?(number)
   end
   true
 end
-
-# def prime?(num)
-#   if num <= 0
-#     return false
-#   elsif num == 2
-#     return true
-#   elsif num % 2 == 0
-#     return false
-#   elsif num <= 7 && (num != 1 && num != 4 && num != 6)
-#     return true
-#   elsif num > 7 && (num % 3 != 0 && num % 5 != 0 && num % 7 != 0)
-#     return true
-#   else
-#     false
-#   end
-# end
-
-
-
-
-
-
-
-# def prime?(num)
-#   if num <= 0 || num == 1
-#     false
-#   elsif num <= 7 && (num == 4 || num == 6)
-#     false
-#   elsif num > 7 && !(num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0)
-#     false
-#   else
-#     true
-#   end
-# end
