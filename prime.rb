cad24bd5d4c7b4e82751de42ddd6300edcf94ba8
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-26 20:10:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(num)
+div = *(2..num-1)
+if num <= 0 || num ==1 
+  return false
+end #end if1
+#len = div.size 
+#counter = 0
+total = []
+div.each{ |element| total << num % element}
+if total.include?(0)
+  return false
+else 
+  return true 
+end #end if2 
+end #end method  
+
+
+
+  
+  
