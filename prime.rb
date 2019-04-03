a5a5d76f70b6c50d1bf81ba0850b0b528b65296f
James Pier <jpier2012@gmail.com>
2019-04-03 12:21:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime? num
+  divis = *(2...num)
+  prime = true
+  prime = false if num <= 1
+
+  for n in divis
+    prime = false if num % n == 0
+  next n
+  end
+
+  prime
+end
