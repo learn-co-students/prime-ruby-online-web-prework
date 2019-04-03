2a3aacfa03f93b82bbe27ad1922b3c791ab08b95
B.A. Hanlon <herbivore137@gmail.com>
2019-04-03 18:21:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  bool = true;
+  if num > 2 
+    arr = (2...num).to_a
+    arr.each do |x|
+      if num % x == 0 
+        bool = false 
+        break
+      end 
+    end
+    bool
+  elsif num == 2 
+    true
+  else
+    false
+  end
+end
