3ceeb84b2690477d263df86ed96dfd14cc459ae9
James Britton <brittonjp@gmail.com>
2019-04-05 12:23:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+ def prime?(n)
+  if n <= 1
+    return false
+  else
+    new_array = (1..n).to_a
+    perfect_divisor_total = 0
+    for number in new_array
+      rem = n % number
+      if rem == 0
+        perfect_divisor_total += 1
+      else
+        next
+      end
+    end
+    if perfect_divisor_total > 2
+        return false
+      else 
+        return true
+    end
+  end
+end 
