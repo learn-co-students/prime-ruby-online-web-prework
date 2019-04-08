6e47ec70dcf2d1b8678f9e49e94c7d269767c1a7
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-08 16:01:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 def prime?(number)
   array = (2...number).to_a
+  # using ... excludes the number from the range
+  # to_a converts the range to an array
   number_of_elements = array.size
   element_number = 0
   if number <= 1
@@ -8,8 +10,10 @@ def prime?(number)
     return true
   end
   while element_number < number_of_elements
+  # number_of_elements is the size of the array
     if number % array[element_number] == 0
       return false
+    # if remainder is 0, not prime
     end
     element_number += 1
   end
