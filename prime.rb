407d3cd0be14fbea66edbf683dc57c989b998d42
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-10 21:13:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime?(numbers)
+  if numbers <= 1
+    return false
+  elsif numbers <= 2
+    return true
+  else (2..numbers/2).none? do |x|
+    numbers % x == 0
+  end
+  end
+end
