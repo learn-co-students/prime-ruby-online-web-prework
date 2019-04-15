70dab3115fa64247828fbcc9e9a242ddb6cd8f50
Ryan Humphries <r.humphries2@gmail.com>
2019-04-15 18:09:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+
+
+  if num <= 1
+    return false
+  end
+
+  (2...num).each do |factor|
+    if (num % factor) == 0
+      return false
+    end
+  end
+
+  true
+end
