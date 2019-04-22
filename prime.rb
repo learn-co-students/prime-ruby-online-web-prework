39e47d8438e465025409341793ae3f66660eff8d
Jess Rezac <jess.rezac@gmail.com>
2019-04-22 00:01:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  if number > 1
+    divisors = (2..Math.sqrt(number)).to_a
+    remainders = []
+    divisors.each do |divisor|
+      remainders.push(number % divisor)
+    end
+    if remainders.index(0) == nil
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
+end
