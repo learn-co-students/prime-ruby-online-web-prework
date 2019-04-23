1b50e06b80a8f63d618ff2ad59d18f52c9590d19
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-23 20:31:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime? (i)
  
  return false if i < 2
  
- (2..i-1).each {|range_number| return false if i % range_number == 0}
+ (2...i).each {|range_number| return false if i % range_number == 0}
  true
 
 end
