389f4661b08b4f4166799f6f029e7cb2f93e3352
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-23 20:27:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime? (i)
+ return false if i < 2
+ return true if i == 2
+ (2..i-1).each {|range_number| return false if i % range_number == 0}
+ true
+
+end
+
+
+#prime numbers can only be divided evenly no remainder by one or itself
+
+#example prime numbers, 2,19,67
+#(should return true)
+
+#not prime numbers, 1, -5, 100, #22 (should return false)
