472e58946cad28db36c26bd23f1f33d2e4d2d9ff
Brian Salemi <briandsalemi@gmail.com>
2019-04-23 14:36:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(n)
+  return false if !n.integer?
+  return false if n < 2
+  return true if n == 2
+  m = (2..n-1)
+  m_range = m.to_a
+  m_range.each {|number| return false if n % number == 0}
+  true
+end
