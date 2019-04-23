b2b382ec102da83a5c319de1450721bbb3109d0a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-23 03:17:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+
+def prime?(num)
+  isPrime = true
+
+  if num < 2
+  isPrime = false
+  else 
+  isPrime = true
+  primep(num)
+end
+end
+
+def primep(num)
+isPrime = true
+  range = (2..Math.sqrt(num)).to_a
+  for counter in range do 
+  
+    if num % counter == 0 
+    isPrime = false
+    
+    end 
+  
+  end
+isPrime
+end
