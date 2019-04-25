545ef17ecb5a119a2f43693eb5e670c654155071
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-25 23:14:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,35 @@
-# Add  code here!
+#uncomment next two lines if benchmarking
+# require 'benchmark'
+# require 'prime'
+
+def prime?(num)
+  if num == 2 || num == 3
+    return true
+  end
+  if num % 2 == 0 || num <= 1
+    return false
+  end
+  testNum = 3
+  loop do
+    quotient = num / testNum
+    if num % testNum == 0
+      return false
+    elsif testNum >= quotient
+      return true
+    end
+    testNum += 2
+  end
+end
+
+#uncomment from  here to the bottom if benchmarking
+=begin
+def check
+  range = (-100..100)
+  for num in range do
+    puts "mine: #{Benchmark.measure { prime?( num ) }}"
+    puts "theirs: #{Benchmark.measure{ Prime.prime?( num )}}"
+  end
+end
+
+check
+=end
