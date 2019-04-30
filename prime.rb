22fdb8cde407d70f6b0273554a3ca4d449959999
Erika Davis <davis2erika@gmail.com>
2019-04-30 22:28:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  (2..number -1).each {|n| return false if number <= 1 || number % n == 0}
+  return true
+end
+
+def prime?(number)
+return false if number <= 1
+  (2..number-1).each do |n|
+  if number % n == 0
+  return false
+   end
+end
+true
+end
