500d6eb7f527325fc4ac62b1d761b35225bd48a1
Jasmine Chen <jasmine1226@gmail.com>
2019-05-07 02:28:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+def prime?(num)
+  count = 2
+  if num <= 0
+    return false
+  else
+    while count < num do
+      if num % count == 0
+        break
+      else
+        count += 1
+      end
+    end
+
+    if count == num
+      return true
+    else
+      return false
+    end
+  end
+end
+
+# puts "Enter a Number:"
+# num = gets.chomp.to_i
+# prime?(num)
