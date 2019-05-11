992c66f1aa3fb699b14c34749484c22b4c610621
Haley DeKeyser <hcd09@live.com>
2019-05-11 16:21:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(n)
+  return false if n < 2
+  test_range = (2..Math.sqrt(n).round).to_a
+  for i in test_range
+    return false if n % i == 0
+  end
+  return true
+end
