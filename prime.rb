093cc0ad3dc40bfed5107cdc2c0d115d5d6ec3fa
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-24 20:16:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,20 @@
-def prime do
-prime.prime
+require "pry"
+def prime?(num)
+if num <= 0
+  return false
+  
+else
+  (2..num-1).to_a
+
+  .all? do |x|
+
+
+  end
+  #(2..9).to_a
+  binding.pry
+  #array of numbers to iterate through
+    #test if the num % each number is 0 (remainder is 0)
+    #return true if all that above is giving 0 for the remainder
+
+end
 end
