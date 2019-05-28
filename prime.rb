56cc9c0b898f164c4c57809b2ed2ecb38aba7eaf
Brendan Robinson <brendanr425@gmail.com>
2019-05-28 20:29:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(int)
+  range = (2...int).to_a
+  count = 0
+  range.each do |idx1|
+    range.each do |idx2|
+      count += 1 if idx2 != idx1 && idx2 % idx1 == 0
+    end
+  end
+  if count == 0
+    return true
+  else
+    return false
+  end
+end
