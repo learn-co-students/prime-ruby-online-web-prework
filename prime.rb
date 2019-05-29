d18f1ea15b226e112014a5c29281fe501eeb5273
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 18:08:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(int)
+  isPrime = false
+  if int <= 1
+    isPrime = false
+  #elsif int == 2 || 3
+  #  isPrime = true
+  else
+    range = 2...int
+    isPrime = true
+
+    for prime_test in range do
+      check1 = int % prime_test
+      if check1 == 0 && int != 2 && int != 3
+        puts "The number is not prime."
+        isPrime = false
+      end
+    end
+  end
+  isPrime
+end
