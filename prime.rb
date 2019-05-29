3d7286c61234a0b411fe0c059858684476b81378
Nathan Rathe <nprathe10@gmail.com>
2019-05-29 23:01:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  range_array = (2..(number - 1)).to_a
+  result = true
+  if number < 2
+    result = false
+  else
+    range_array.each do |r|
+      if number % r == 0
+        result = false
+        break
+      end
+    end
+  end
+  return result
+end
