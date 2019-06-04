ea318364a16da8077e6f8b0098946492e1a1a094
Nick Baughman <nbaugh1@gmail.com>
2019-06-04 01:25:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  if integer == 0
+    return false
+  elsif integer == 1
+    return false
+  elsif integer == -1
+    return false
+  end
+  (2..integer - 1).each {|x| return false if (integer % x) == 0}
+  true
+end
