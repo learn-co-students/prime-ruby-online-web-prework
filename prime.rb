bd4d47568f10cea2f605fdc2d137056ec383e246
Amber Taranto <ambzzzz@gmail.com>
2019-06-05 01:44:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  return false if integer <= 1
+
+  number = 2
+
+  while number < integer
+    if integer % number == 0
+      return false
+    end
+    number += 1
+  end
+  return true
+end
