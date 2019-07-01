70cb929392fd8bd77a499d299e81d1fd8bc98e37
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-07-01 01:49:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  number_aux = 0
+  (1..number).each do |n|
+     if number % n == 0
+      number_aux += 1
+     end
+  end
+  if number_aux == 2
+    true 
+  else
+    false 
+  end
+  #Muestra si el numero dado solo tiene dos divisores, como debe ser para un número primo
+end
