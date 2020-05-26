25f58cf161f132e0a662060d5ef7041c2cb655fa
fasnimo <fasnimo@gmail.com>
2020-05-25 23:18:25 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,10 +1,5 @@
-def prime?(integer)
- if integer < 2 
-   return false
-else   
- (2...integer-1).to_a.all? do |solve|
-    integer % solve != 0
-     
-   end
- end
-end
+require "prime"
+
+def prime?(array)
+    Prime.prime?(array)
+end 
